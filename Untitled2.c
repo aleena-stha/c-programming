2b294e4396d2533cc0f0279781d6b229ac3a12cf
aleena-stha <159105068+aleena-stha@users.noreply.github.com>
2025-01-22 22:27:09 +0545

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,14 @@
+// to display 10 natural number in reverse order
+#include<stdio.h>
+int main()
+{
+	int i=10;
+	while(i>=1)
+	
+	{
+	 printf(" %d\n",i);
+	  i=i-1;
+    }  
+    printf("\n");
+    return 0;
+}
